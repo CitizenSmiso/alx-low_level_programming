d773937a406f9a98dc3009836352004d39d2bef7
CitizenSmiso <Citzensmiso.03@gmail.com>
2022-09-08 14:56:40 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio.h>
-#include <unistd.h>
-
+#include<unistd.h>
 /**
- *  * main - Entry point
+ *
+ *  * main - prints the standrad error sandwiched around the  standard outpu/t
+ *
  *   *
- *    * Return: always 1 (success)
+ *
+ *    * Return: returns 1
+ *
  *     */
 int main(void)
 {
-		write(2,
-				"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
 /**
  *
- *  * main - main block
+ *  *main - entry point
  *
- *   * Return: 0
+ *   *
  *
- *    */
-
+ *    *Return: 0 after printing the function
+ *
+ *     */
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
