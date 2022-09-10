68a02ba8fe62413d8af8635a7dfc33464cee7263
CitizenSmiso <Citzensmiso.03@gmail.com>
2022-09-09 17:03:46 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (i = 0; i < 100; p++)
+	{
+		for (p = 0; p < 100; p++)
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
+				if (i != 98 || p != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+	}
+}
+putchar('\n');
+return (0);
+}
